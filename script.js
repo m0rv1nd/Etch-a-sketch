18acbefbdc9ed6d35aad4ed1ff9692901ccf1c69
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2024-12-19 11:27:50 +0100

    add draw feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let gridSize = +prompt("Enter the grid size");
 let gridContainer = gridSize * gridSize;
 const container = document.querySelector("#container");
+let ifClick = false;
 
 
 for (i = 0; i <= gridContainer - 1; i++) {
@@ -14,6 +15,24 @@ const grid = document.querySelectorAll(".grid");
 for (let div of grid) {
     div.style.width = 960 / gridSize + "px";
     div.style.height = 960 / gridSize + "px";
+
+    div.addEventListener("mousedown", (event) => {
+        if (event.button === 0) {
+            ifClick = true;
+        }
+    });
+
+    div.addEventListener("mousemove", () => {
+        if (ifClick) {
+            div.style.backgroundColor = "yellow";
+        }
+    });
+
+    div.addEventListener("mouseup", () => {
+        ifClick = false;
+        }
+    );
 }
+
 
 
