e31badb227a9c9c2e495e74d1c19facb8c46b209
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2024-12-19 10:25:24 +0100

    add grid resizing

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+let gridSize = +prompt("Enter the grid size");
+let gridContainer = gridSize * gridSize;
 const container = document.querySelector("#container");
 
-for (i = 0; i <= 255; i++) {
+
+for (i = 0; i <= gridContainer - 1; i++) {
     const newDiv = document.createElement("div");
+    newDiv.classList.add("grid");
     container.appendChild(newDiv);
 } 
+
+const grid = document.querySelectorAll(".grid");
+
+for (let div of grid) {
+    div.style.width = 960 / gridSize + "px";
+    div.style.height = 960 / gridSize + "px";
+}
+
+
